456044b2e3c2380bbf6fa940860c44bac06f0aab
reyansh2505 <102719517+reyansh2505@users.noreply.github.com>
2022-05-29 10:16:49 +0530

    Add files via upload

---
+++ calcius to far..c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+int c;
+float f;
+printf("enter the value of c");
+scanf("%d",&c);
+f=(c*(9/5))+32;
+printf("the value of f is: %f",f);
+return 0;
+}
